c6534d8ff1322dac280ca35bfd374c571526cf9f
PixelVoid <Pixel0Void@gmail.com>
2025-04-24 16:24:11 +0430

    feat: add blueprint and set target object to build in scene

---
+++ Assets/Scripts/Blueprint.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Blueprint : MonoBehaviour
+{
+    public GameObject Floor;
+    public KeyCode FloorHotkey;
+
+    public GameObject Wall;
+    public KeyCode WallHotkey;
+
+    public GameObject Window;
+    public KeyCode WindowHotkey;
+
+    public GameObject Door;
+    public KeyCode DoorHotkey;
+
+    public KeyCode RemoveHotkey;
+
+    [SerializeField] private GameObject m_TargetObj;
+
+    public void SetTargetObject()
+    {
+        if(Input.GetKeyDown(FloorHotkey))
+        {
+            SetTargetObject(Floor);
+        }
+        else if (Input.GetKeyDown(WallHotkey))
+        {
+            SetTargetObject(Wall);
+        }
+        else if (Input.GetKeyDown(WindowHotkey))
+        {
+            SetTargetObject(Window);
+        }
+        else if (Input.GetKeyDown(DoorHotkey))
+        {
+            SetTargetObject(Door);
+        }
+        else if(Input.GetKeyDown(RemoveHotkey))
+        {
+            SetTargetObject(null);
+        }
+    }
+
+    private void SetTargetObject(GameObject obj)
+    {
+        m_TargetObj = obj;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private bool m_InEditMode = false;
     private CameraController m_CameraController;
     public BlueprintPanel BlueprintPanel;
+    public Blueprint Blueprint;
 
     private void Awake()
     {
@@ -18,6 +19,11 @@ public class GameManager : MonoBehaviour
             m_InEditMode = !m_InEditMode;
             m_CameraController.InOutEditMode(m_InEditMode);
             BlueprintPanel.OpenClosePanel(m_InEditMode);
+        }
+
+        if(m_InEditMode)
+        {
+            Blueprint.SetTargetObject();
         }
     }
 }
