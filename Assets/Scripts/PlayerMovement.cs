558e6ad8698f708f3349de45671459bd1cc4ef37
PixelVoid <Pixel0Void@gmail.com>
2025-04-24 14:04:41 +0430

    feat: add basic player movement, add player material

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 6f;
+    private CharacterController m_Controller;
+
+    private void Awake()
+    {
+        m_Controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        m_Controller.Move(move * Speed * Time.deltaTime);
+    }
+}
