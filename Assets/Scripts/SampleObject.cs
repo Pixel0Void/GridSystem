4421320d42ee1f5a11f9555a42c5beeaee8a1cf4
PixelVoid <Pixel0Void@gmail.com>
2025-04-24 16:54:10 +0430

    feat: add sample object for preview

---
+++ Assets/Scripts/SampleObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SampleObject : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Floor;
+    [SerializeField] private GameObject m_Wall;
+    [SerializeField] private GameObject m_Window;
+    [SerializeField] private GameObject m_Door;
+
+    public void SetActive(BuildingsEnum building)
+    {
+        m_Floor.SetActive(false);
+        m_Wall.SetActive(false);
+        m_Window.SetActive(false);
+        m_Door.SetActive(false);
+
+        switch (building)
+        {
+            case BuildingsEnum.Floor:
+                m_Floor.SetActive(true);
+                break;
+            case BuildingsEnum.Wall:
+                m_Wall.SetActive(true);
+                break;
+            case BuildingsEnum.Window:
+                m_Window.SetActive(true);
+                break;
+            case BuildingsEnum.Door:
+                m_Door.SetActive(true);
+                break;
+            case BuildingsEnum.Remove:
+                break;
+        }
+    }
+}
