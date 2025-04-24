544e8f4ea7299e46d2571c63c49ba15013c1a105
PixelVoid <Pixel0Void@gmail.com>
2025-04-24 14:57:01 +0430

    feat: add camera following player, add ground material

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float Speed = 5f;
+    private Transform m_Target;
+
+    private void Awake()
+    {
+        m_Target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = new Vector3(m_Target.position.x, transform.position.y, m_Target.transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Speed * Time.deltaTime);
+    }
+}
