b2a0ed7e26cf0c888002893eb5be9d7fbb95749a
PixelVoid <Pixel0Void@gmail.com>
2025-04-24 14:06:42 +0430

    feat: add convertion method for inputs to isometric

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,16 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        move = IsoVectorConvert(move);
+
         m_Controller.Move(move * Speed * Time.deltaTime);
+    }
+
+    private Vector3 IsoVectorConvert(Vector3 vector)
+    {
+        Quaternion rotation = Quaternion.Euler(0f, 45f, 0f);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotation);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
     }
 }
