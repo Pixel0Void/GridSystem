8b6973977deea5977df183d56c371fef2b087d36
PixelVoid <Pixel0Void@gmail.com>
2025-04-26 11:26:35 +0430

    feat: add struct just for data that is needed

---
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+[Serializable]
+public struct SaveData
+{
+    public string Name;
+    public int XPosition;
+    public int ZPosition;
+    public float YRotation;
+
+    public SaveData(string name, int x, int z, float yRotation)
+    {
+        Name = name;
+        XPosition = x;
+        ZPosition = z;
+        YRotation = yRotation;
+    }
+}
