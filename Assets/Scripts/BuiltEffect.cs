2c2fc9442e66adf968e54d5f278f77c26cc8e573
PixelVoid <Pixel0Void@gmail.com>
2025-04-27 14:15:39 +0430

    feat: add a smooth movement to objects when they built

---
+++ Assets/Scripts/BuiltEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BuiltEffect : MonoBehaviour
+{
+    private float m_Height = 1f;
+    private float m_Speed = 3f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, m_Height, transform.position.z);
+        InvokeRepeating(nameof(ShowEffect), 0f, 0.01f);
+    }
+
+    private void ShowEffect()
+    {
+        transform.Translate(Vector3.down * m_Speed * Time.deltaTime);
+        if(transform.position.y <= 0f)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+            CancelInvoke(nameof(ShowEffect));
+            Destroy(this);
+        }
+    }
+}
